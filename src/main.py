5935e7efb0d950103bdebf86a79bb748f5e5e8fc
Kyle Miskell <kmiskell@protonmail.com>
2026-07-29 12:50:29 -0400

    add in complex path which uses path param, query param, and request body to get item, filter if item is valid for update, then update

--- src/main.py
+++ src/main.py
@@ -7,11 +7,10 @@ from enum import Enum
 app = FastAPI()
 
 class Item(BaseModel):
+    id: int
     name: str
     price: float
     is_offer: bool | None = None
-    mixed_type_tuple: tuple[int, int, str] | None = None
-    name_to_num: dict[str, int] | None = None
     
 class Color(str, Enum):
     RED = "red"
@@ -19,10 +18,10 @@ class Color(str, Enum):
     GREEN = "green"
     
 test_items: list[Item] = [
-    Item(name="Apple", price=0.41, is_offer=True),
-    Item(name="Pear", price=0.49, is_offer=False),
-    Item(name="Pineapple", price=2.49, is_offer=False),
-    Item(name="Peach", price=0.57, is_offer=False)
+    Item(id=1, name="Apple", price=0.41, is_offer=True),
+    Item(id=2, name="Pear", price=0.49, is_offer=False),
+    Item(id=3, name="Pineapple", price=2.49, is_offer=False),
+    Item(id=4, name="Peach", price=0.57, is_offer=False)
 ]
     
 # Most basic GET path
@@ -53,10 +52,25 @@ async def update_item(item_id: int, item: Item):
 
 # Path which only allows getting specific color names
 @app.get("/item-color/{color_id}")
-async def get_color(color_id: Color):
+async def get_item_color(color_id: Color):
     return {"color_name": color_id.name}
 
 # Path which uses a query param to filter items chepaer than max_price
 @app.get("/items/")
 async def get_cheap_items(max_price: float):
     return list(filter(lambda item: item.price < max_price, test_items))
+
+# Path which uses path param, query param, and request body
+# Path param gets item, query param filters item, request body gives update data
+# Really don't need to pass in whole item here, but works for tutorial purposes
+@app.patch("/items/{item_id}")
+async def set_offer_if_item_expensive(item_id: int, item: Item, expensive_price: float) -> Item:
+    for test_item in test_items:
+        if test_item.id == item_id:
+            if test_item.price > expensive_price:        
+                test_item.is_offer = True
+                test_item.price = item.price
+                
+            return test_item
+        
+    raise HTTPException(status_code=404, detail="Item not found")
