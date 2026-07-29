3d2efc112ad040dd30f19f1a7624970775e46ed9
Kyle Miskell <kmiskell@protonmail.com>
2026-07-29 11:43:12 -0400

    Add in path param to tutorial code

--- src/main.py
+++ src/main.py
@@ -5,9 +5,13 @@ from pydantic import BaseModel
 from enum import Enum
 
 app = FastAPI()
-]
 
 # Most basic GET path
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+# Path takes path parameter to ID resource
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
